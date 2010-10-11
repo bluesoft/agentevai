639525838edfb8499e20c927a37e42a144ee66df
André Faria <andre.java@gmail.com>
2010-10-11 15:37:37 -0300

    adding capistrano

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -8,21 +8,7 @@ function goTo(url){
 
 function createMenu(){
 	$("#menu").buildMenu({
-      additionalData:"pippo=1",
-      menuWidth:200,
-      openOnRight:false,
-      menuSelector: ".menuContainer",
       iconPath:"ico/",
-      hasImages:true,
-      fadeInTime:100,
-      fadeOutTime:300,
-      adjustLeft:2,
-      minZindex:"auto",
-      adjustTop:10,
-      opacity:.95,
-      shadow:true,
-      openOnClick:true,
-      closeOnMouseOut:true,
-      closeAfter:1000
+      hasImages:true
     });
 }
