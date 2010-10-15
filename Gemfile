74c5b2c388296662c87b6a0bbd27bd52afd423e2
André Faria <andre.java@gmail.com>
2010-10-15 11:23:39 -0300

    adding inploy

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'rails', '3.0.0'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
+gem 'inploy'
 gem 'pg'
 gem 'sqlite3-ruby', :require => 'sqlite3'
 gem 'inherited_resources', '1.1.2'
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,20 +1,14 @@
-set :application, "A Gente Vai!"
-set :repository,  "git@front:agentevai.git"
-set :scm, :git
+application = "agentevai"
+repository = 'git@front:agentevai.git'
+hosts = %w('server')
+template = :rails3_push
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-#role :db,  "your slave db-server here"
 
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-namespace :deploy do
-   task :start do ; end
-   task :stop do ; end
-   task :restart, :roles => :app, :except => { :no_release => true } do
-     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-   end
- end
+#path = '/opt'                                # default /var/local/apps
+user = 'bluesoft'                            # default deploy
+#ssh_opts = '-A'                             # default empty
+#branch = 'master'                           # default master
+#sudo = true                                 # default false
+#cache_dirs = ['public/cache', 'tmp/cache']   # default ['public/cache']
+#skip_steps = ['install_gems', 'clear_cache'] # default []
+#app_folder = 'project_folder'                # default empty
